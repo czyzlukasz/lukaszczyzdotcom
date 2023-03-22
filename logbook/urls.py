f7b0ae9a20985e30f465a66ba1c9ec5047a49282
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-03-22 21:27:37 +0100

    Links in project list now redirect to detail view of the project

--- logbook/urls.py
+++ logbook/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'logbook'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.ProjectView.as_view(), name='project')
