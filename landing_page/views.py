ab3ce7d7306e3bc1754dd285ba26c5eb700c6823
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-03-22 22:00:45 +0100

    Added listing of log entries of given project, latest logs are being displayed on landing page

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from logbook.models import Project
+from logbook.models import Project, LogEntry
 
 def index(request):
     best_projects = Project.objects.all()[0:3]
-    return render(request, 'landing_page/index.html', context={'projects': best_projects})
+    last_log_entries = LogEntry.objects.all()[0:3]
+    return render(request, 'landing_page/index.html', context={'projects': best_projects, 'log_entries': last_log_entries})
--- logbook/views.py
+++ logbook/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 
-from .models import Project
+from .models import Project, LogEntry
 
 
 def index(request):
@@ -12,3 +12,9 @@ class ProjectView(DetailView):
     model = Project
 
     context_object_name = 'project'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        log_entries = LogEntry.objects.filter(project = context['project'])
+        context['log_entries'] = log_entries
+        return context
