e85c0768740efae33112f4120592983576b2ce8c
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-03-22 21:22:21 +0100

    Added detail view for projects

--- logbook/views.py
+++ logbook/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Project
+
+
+def index(request):
+    return HttpResponse('elo')
+
+class ProjectView(DetailView):
+    model = Project
+
+    context_object_name = 'project'
--- lukaszczyzdotcom/urls.py
+++ lukaszczyzdotcom/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('landing_page.urls'))
+    path('', include('landing_page.urls')),
+    path('logbook/', include('logbook.urls'))
 ]
 
 if settings.DEBUG:
