5dfb21b6a3f116dd1263e82ad043e44de82656d2
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-03-22 18:27:26 +0100

    Landing page displays top projects

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from logbook.models import Project
 
 def index(request):
-    return render(request, 'landing_page/index.html')
+    best_projects = Project.objects.all()[0:3]
+    return render(request, 'landing_page/index.html', context={'projects': best_projects})
