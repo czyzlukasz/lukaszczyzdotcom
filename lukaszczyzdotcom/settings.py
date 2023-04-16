65ea5a972ee205010248833ef3175a9db48c2502
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-04-16 21:28:04 +0200

    Set ALLOWED_HOSTS to local address only

--- lukaszczyzdotcom/settings.py
+++ lukaszczyzdotcom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-l#2g^x$m8yq8u74bp()_c!6+dbozycnhb@*$#9g47oqpbqv!d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.170"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
