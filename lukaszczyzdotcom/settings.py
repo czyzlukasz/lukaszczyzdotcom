2d52466ecf9d02584272fc472f9c5110385cbd81
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-04-23 22:49:22 +0200

    Debug is turned off by default

--- lukaszczyzdotcom/settings.py
+++ lukaszczyzdotcom/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT= BASE_DIR / MEDIA_URL
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
