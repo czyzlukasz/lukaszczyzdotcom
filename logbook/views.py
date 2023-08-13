ba92131458f260b4cc113cd44403d8e7c4ecb92a
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-08-13 12:13:28 +0200

    Log entries in project show from oldest to newest

--- logbook/views.py
+++ logbook/views.py
@@ -16,7 +16,7 @@ class ProjectView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        log_entries = LogEntry.objects.filter(project = context['project'])
+        log_entries = LogEntry.objects.filter(project = context['project']).order_by('publish_date')
         context['log_entries'] = log_entries
         return context
     
