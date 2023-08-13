dd0a21e3d2d491618d8b6df895a5bc3c6e319eb2
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-08-13 12:09:55 +0200

    Added project list-like padding to about section

--- lukaszczyzdotcom/settings.py
+++ lukaszczyzdotcom/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT= BASE_DIR / MEDIA_URL
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ["localhost"]
